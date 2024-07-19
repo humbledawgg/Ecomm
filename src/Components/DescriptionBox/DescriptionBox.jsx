5af44dcce59ad03dfc3570cadb777b9a325cd254
humbledawgg <chester.gohan@gmail.com>
2024-07-19 12:27:13 +0530

    first commit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className='descriptionbox-description'>
+      <p>Ecommerce businesses are experts at creating a seamless customer experience. As an ecommerce business, your resume should have a lot in common with your website.
+       It should be easy to navigate, visually appealing, and tailored to the needs of the customer. In this guide, we'll review X ecommerce resume examples that are sure
+       to make a great impression.</p>
+       <p>
+       Ecommerce is a rapidly growing field that requires a unique blend of technical, business, and marketing skills to succeed. Ecommerce professionals are responsible for developing and managing online stores,
+        driving customer engagement, and optimizing the customer experience.
+        To secure a job in the ecommerce industry, it’s essential to position yourself effectively through a resume that fully communicates your value and potential as an ecommerce professional.
+       </p>
+      </div>
+      
+    </div>
+  )
+}
+
+export default DescriptionBox
